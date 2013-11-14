68cd75dce3d4a30611f3e9e1232f79dd858e0824
Zach Vega-Perkins <vegaperk@usc.edu>
2013-11-13 19:11:39 -0800

    Changed resident to alter dropbox directly. Implemented basic messages, scheduler, and actions.

--- src/housing/ResidentRole.java
+++ src/housing/ResidentRole.java
@@ -1,24 +1,58 @@
 package housing;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+
 import housing.interfaces.Resident;
 import agent.Role;
 
 public class ResidentRole extends Role implements Resident {
-
+	double moneyOwed = 0;
+	Map<Resident, Double> landlordDropbox = Collections.synchronizedMap(new HashMap<Resident, Double>());
+	
+	Map<String, Food> refrigerator;
+	Food food; //the food the resident is currently eating
+	Timer timer; //used to cook food and time eat period
+	Map<String, Integer> cookTimes;
+	
+	enum FoodState { COOKED };
+	class Food{
+		String type;
+		FoodState state;
+		int amount, cookTime, low, capacity;
+	}
+	
 	public ResidentRole() {
-		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	/** Messages */
+	@Override
+	public void msgPaymentDue(double amount, Map<Resident, Double> d) {
+		this.moneyOwed = amount;
+		this.landlordDropbox = d;
 	}
 
+	/** Scheduler */
 	@Override
 	protected boolean pickAndExecuteAnAction() {
-		// TODO Auto-generated method stub
+		if(moneyOwed > 0){
+			makePayment();
+		}
+		if(food != null && food.state == FoodState.COOKED){
+			
+		}
 		return false;
 	}
-
-	@Override
-	public void msgPaymentDue(double amount, int accountNumber) {
-		// TODO Auto-generated method stub
-		
+	
+	/** Actions */
+	private void makePayment(){
+		landlordDropbox.put(this, moneyOwed);
+//		money -= moneyOwed;
+		moneyOwed = 0;
 	}
-
 }
--- src/housing/interfaces/Resident.java
+++ src/housing/interfaces/Resident.java
@@ -1,7 +1,9 @@
 package housing.interfaces;
 
+import java.util.Map;
+
 public interface Resident {
 	/** Normative Messages from Payment Collector
 	 * Examples: landlord, bank */
-	public void msgPaymentDue(double amount, int accountNumber);
+	public void msgPaymentDue(double amount, Map<Resident, Double> dropBox);
 }

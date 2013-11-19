9fc62f5733d483fb9ee0b890d2487f343b160b5d
Zach Vega-Perkins <vegaperk@usc.edu>
2013-11-18 22:53:55 -0800

    Implemented eating action and event.

--- src/housing/ResidentRole.java
+++ src/housing/ResidentRole.java
@@ -1,24 +1,29 @@
 package housing;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Timer;
+import java.util.TimerTask;
 
 import housing.interfaces.Resident;
 import agent.Role;
 
 public class ResidentRole extends Role implements Resident {
+	/** Rent Data */
 	double moneyOwed = 0;
 	Map<Resident, Double> landlordDropbox = Collections.synchronizedMap(new HashMap<Resident, Double>());
 	
+	/** Food Data */
 	Map<String, Food> refrigerator;
 	Food food; //the food the resident is currently eating
-	Timer timer; //used to cook food and time eat period
-	Map<String, Integer> cookTimes;
+	Timer timer = new Timer(); //used to cook food and time eat period
+	Map<String, Integer> cookTimes = Collections.synchronizedMap(new HashMap<String, Integer>());
 	
+	/** Constant Variables */
+	private final int EAT_TIME = 6; 
+	
+	/** Class Data */
 	enum FoodState { COOKED };
 	class Food{
 		String type;
@@ -44,7 +49,7 @@ public class ResidentRole extends Role implements Resident {
 			makePayment();
 		}
 		if(food != null && food.state == FoodState.COOKED){
-			
+			eatFood();
 		}
 		return false;
 	}
@@ -54,5 +59,22 @@ public class ResidentRole extends Role implements Resident {
 		landlordDropbox.put(this, moneyOwed);
 //		money -= moneyOwed;
 		moneyOwed = 0;
+	}
+	private void eatFood(){
+//		TODO: ANIMATION DETAILS
+//		DoGoToStove();
+//		acquire(performingTasks);
+//		DoGoToTable();
+//		acquire(performingTasks);
+//		DoEatFood();
+//		acquire(performingTasks);
+		
+		timer.schedule(new TimerTask() {
+			public void run() {
+				food = null;
+				stateChanged();
+			}
+		},
+		EAT_TIME * 1000);//how long to wait before running task
 	}
 }

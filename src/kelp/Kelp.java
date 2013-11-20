0da6f9e7cf00e056fda03199eea79085e7da7b90
Diego von Beck <dvonbeck@gmail.com>
2013-11-19 20:27:16 -0800

    Fixed some Kelp problems

--- src/kelp/Kelp.java
+++ src/kelp/Kelp.java
@@ -1,24 +1,28 @@
 package kelp;
 
 import java.util.List;
-import java.util.SortedMap;
-
 import CommonSimpleClasses.*;
 import CommonSimpleClasses.CityLocation.LocationTypeEnum;
 
-// TODO Fix image likn in kelp.md on my documents
+// TODO Fix image link in kelp.md on my documents
 // TODO add to interface new public methods
 // TODO update DD
 public interface Kelp {
-	
-	public List<CityLocation> routeFromAToB(XYPos A, CityLocation B);
-	
-	public List<CityLocation> routeFromAToB(CityLocation A, CityLocation B);
+	// TODO Update DD
+	public List<CityLocation> routeFromAToB(XYPos A, CityLocation B,
+			Boolean tryBus);
 	
 	// TODO Update DD
-	public SortedMap<Integer, CityLocation> placesNearMe(XYPos me, LocationTypeEnum type);
+	public List<CityLocation> routeFromAToB(CityLocation A,
+			CityLocation B, Boolean tryBus);
+	
+	// TODO Update DD
+	public List<CityLocation> placesNearMe(XYPos me,
+			LocationTypeEnum type);
 	
 	
 	// TODO Update DD
-	public SortedMap<Integer, CityLocation> placesNearMe(CityLocation me, LocationTypeEnum type);
+	public List<CityLocation> placesNearMe(CityLocation me,
+			LocationTypeEnum type);
+
 }
--- src/kelp/KelpClass.java
+++ src/kelp/KelpClass.java
@@ -1,45 +1,86 @@
 package kelp;
 
-import java.awt.Point;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedMap;
-import java.util.TreeMap;
-
-import javax.management.modelmbean.InvalidTargetObjectTypeException;
-import javax.naming.InitialContext;
-
 import transportation.interfaces.Busstop;
 import transportation.interfaces.Corner;
-
-import com.sun.org.apache.xml.internal.security.utils.IgnoreAllErrorHandler;
-
 import CommonSimpleClasses.CityBuilding;
 import CommonSimpleClasses.CityLocation;
 import CommonSimpleClasses.DirectionEnum;
 import CommonSimpleClasses.CityLocation.LocationTypeEnum;
 import CommonSimpleClasses.XYPos;
 
-// TODO Make methods return something other than maps to avoid overwriting
 public class KelpClass implements Kelp {
 	
 	// TODO SET CORRECT CONSTANTS
-	private static final double AVG_BUS_WAIT = 30;
-	private static final double BUS_SPEED = 0; 
-	private static final double WALKING_SPEED = 0;
-	private static final int DISTANCE_THRESHOLD_FOR_SAME_COORDINATE = 0;
+	private static final double AVG_BUS_WAIT = 15;
+	private static final double BUS_SPEED = 80; 
+	private static final double WALKING_SPEED = 30;
+	private static final int DISTANCE_THRESHOLD_FOR_SAME_COORDINATE = 8;
 	List<CityLocation> locations;
 	
 	List<Corner> busRoute;
+	
+	static KelpClass instance = null;
+	
+	static KelpClass getKelpInstance() throws Exception {
+		if (instance != null) return instance;
+		else throw new Exception("Tried to grab a Kelp instance "
+				+ "without it having been instantiated.");
+	}
+	
+	public KelpClass(List<CityLocation> locations, 
+			List<Corner> busRoute) throws Exception {
+		
+		if (instance == null) {
+			this.locations = new ArrayList<CityLocation> (locations);
+			this.busRoute = new ArrayList<Corner> (busRoute);
+			instance = this;
+		} else {
+			throw new Exception("Tried to create a second instance of Kelp, "
+					+ "Kelp is a singleton.");
+		}
+	}
 
 	@Override
-	public List<CityLocation> routeFromAToB(XYPos A, CityLocation B) {
-		boolean tryBus = true;  //TODO Add as argument, assuming correct input
-		boolean hasCar = false;  //TODO Add as argument, assuming no car
+	public List<CityLocation> placesNearMe
+								(XYPos me, LocationTypeEnum type) {
+		
+		List<CityLocation> response = 
+				new ArrayList<CityLocation>();
+		
+		for (CityLocation loc : locations) if (loc.type() == type) {
+			if (response.size() == 0) {
+				response.add(loc);
+			} else {
+				boolean inserted = false;
+				for (int i = 0; i < response.size(); i++){
+					if (distanceHeuristic(me, response.get(i).position())
+							> distanceHeuristic(me, loc.position()) ) {
+						response.add(i, loc);
+						inserted = true;
+						break;
+					}
+				}
+				if (!inserted) response.add(loc);
+			}
+		}
+		
+		return response;
+	}
+
+	@Override
+	public List<CityLocation> placesNearMe(CityLocation me,
+			LocationTypeEnum type) {
+		return placesNearMe(me.position(), type);
+	}
+
+	@Override
+	public List<CityLocation> routeFromAToB(XYPos A, 
+			CityLocation B, Boolean tryBus) {
 		
 		XYPos posA = A;
-		XYPos posB = getEntrancePosition(B);
+		XYPos posB = getAbsoluteEntrancePosition(B);
 		
 		List<CityLocation> response;
 		
@@ -57,6 +98,13 @@ public class KelpClass implements Kelp {
 		return response;
 	}
 
+	@Override
+	public List<CityLocation> routeFromAToB(CityLocation A, 
+			CityLocation B, Boolean tryBus){
+		XYPos posA = getAbsoluteEntrancePosition(A);
+		return routeFromAToB(posA, B, tryBus);
+	}
+
 	private List<CityLocation> returnFastestPath(
 			List<CityLocation> path1, List<CityLocation> path2, 
 			XYPos initialPos) {
@@ -70,6 +118,7 @@ public class KelpClass implements Kelp {
 		} else return path2;
 	}
 
+	// TODO Make accurate when car?
 	public double timeForPath(List<CityLocation> path, XYPos initialPos) {
 		XYPos pastPos = initialPos;
 		boolean wasBusstop = false;
@@ -88,7 +137,7 @@ public class KelpClass implements Kelp {
 					speed = WALKING_SPEED;
 				}
 				time += distanceHeuristic(pastPos, loc.position())
-						* speed;
+						/ speed;
 				pastPos = loc.position();
 				wasBusstop = false;
 			}
@@ -97,14 +146,15 @@ public class KelpClass implements Kelp {
 	}
 
 	private List<CityLocation> routeWithBus(XYPos posA, XYPos posB) {
-		SortedMap<Integer, CityLocation> nearBusstops = placesNearMe(posA, 
+		List<CityLocation> nearBusstops = placesNearMe(posA, 
 				LocationTypeEnum.Busstop);
-		Busstop nearestBusstop = (Busstop)nearBusstops.get(nearBusstops.firstKey());
+		Busstop nearestBusstop = 
+				(Busstop)nearBusstops.get(0);
 		Corner nearestCornerWithBusstopStart = nearestBusstop.corner();
 		
 		nearBusstops = placesNearMe(posB, 
 				LocationTypeEnum.Busstop);
-		nearestBusstop = (Busstop)nearBusstops.get(nearBusstops.firstKey());
+		nearestBusstop = (Busstop)nearBusstops.get(0);
 		Corner nearestCornerWithBusstopEnd = nearestBusstop.corner();
 		
 		List<CityLocation> path = new ArrayList<CityLocation>();
@@ -141,42 +191,20 @@ public class KelpClass implements Kelp {
 		return path;
 	}
 	
-	// TODO make it use actual distance?
-	private boolean isBusstopDistancePositive(
-			Corner nearestCornerWithBusstopStart,
-			Corner nearestCornerWithBusstopEnd) throws Exception {
-		int startIndex = busRoute.indexOf(nearestCornerWithBusstopStart);
-		int endIndex = busRoute.indexOf(nearestCornerWithBusstopEnd);
-		
-		if (startIndex < 0 || endIndex < 0) {
-			throw new Exception("isBusstopDistancePositiveOrNegative "
-					+ "method received a corner that is not part of"
-					+ "the bus route");
-		}
-		
-		if (((endIndex - startIndex) % busRoute.size())
-				<= busRoute.size()/2) {
-			return true;
-		} else return false;
-				
-	}
-
 	private List<CityLocation> routeWithoutBus(XYPos posA, XYPos posB) {
-		SortedMap<Integer, CityLocation> nearCorners = placesNearMe(posA, 
+		List<CityLocation> nearCorners = placesNearMe(posA, 
 				LocationTypeEnum.Corner);
 		
 		//Getting the 2 corners closest to A
 		List<Corner> cornersInit = new ArrayList<Corner>();
-		cornersInit.add((Corner)nearCorners.get(nearCorners.firstKey()));
-		nearCorners.remove(nearCorners.firstKey());
-		cornersInit.add((Corner)nearCorners.get(nearCorners.firstKey()));
+		cornersInit.add((Corner)nearCorners.get(0));
+		cornersInit.add((Corner)nearCorners.get(1));
 		
 		//Getting the 2 corners closest to B
 		nearCorners = placesNearMe(posB, LocationTypeEnum.Corner);
 		List<Corner> cornersEnd = new ArrayList<Corner>();
-		cornersEnd.add((Corner)nearCorners.get(nearCorners.firstKey()));
-		nearCorners.remove(nearCorners.firstKey());
-		cornersEnd.add((Corner)nearCorners.get(nearCorners.firstKey()));
+		cornersEnd.add((Corner)nearCorners.get(0));
+		cornersEnd.add((Corner)nearCorners.get(1));
 		
 		
 		//Determining shortest path
@@ -233,38 +261,32 @@ public class KelpClass implements Kelp {
 		return path;
 	}
 
-	@Override
-	public List<CityLocation> routeFromAToB(CityLocation A, CityLocation B) {
-		XYPos posA = getEntrancePosition(A);
-		return routeFromAToB(posA, B);
-	}
-
-	@Override
-	public SortedMap<Integer, CityLocation> placesNearMe
-								(XYPos me, LocationTypeEnum type) {
+	// TODO make it use actual distance instead of manhattan?
+	private boolean isBusstopDistancePositive(
+			Corner nearestCornerWithBusstopStart,
+			Corner nearestCornerWithBusstopEnd) throws Exception {
+		int startIndex = busRoute.indexOf(nearestCornerWithBusstopStart);
+		int endIndex = busRoute.indexOf(nearestCornerWithBusstopEnd);
 		
-		SortedMap<Integer, CityLocation> response = 
-				new TreeMap<Integer, CityLocation>();
-		
-		for (CityLocation loc : locations) if (loc.type() == type) {
-			response.put(distanceHeuristic(me, getEntrancePosition(loc)),loc);
+		if (startIndex < 0 || endIndex < 0) {
+			throw new Exception("isBusstopDistancePositiveOrNegative "
+					+ "method received a corner that is not part of"
+					+ "the bus route");
 		}
 		
-		return response;
+		if (((endIndex - startIndex) % busRoute.size())
+				<= busRoute.size()/2) {
+			return true;
+		} else return false;
+				
 	}
 
-	@Override
-	public SortedMap<Integer, CityLocation> placesNearMe(CityLocation me,
-			LocationTypeEnum type) {
-		return placesNearMe(me.position(), type);
-	}
-	
 	private int distanceHeuristic(XYPos A, XYPos B) {
 		return Math.abs(A.x - B.x) + Math.abs(A.y - B.y);
 	}
 	
 	
-	private XYPos getEntrancePosition(CityLocation loc) {
+	private XYPos getAbsoluteEntrancePosition(CityLocation loc) {
 		if (loc.type() == LocationTypeEnum.Corner ||
 			loc.type() == LocationTypeEnum.Busstop) {
 			return loc.position();

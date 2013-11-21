b88f8a6ccac619b7be1bf98882728f7dc588bd2d
Zach Vega-Perkins <vegaperk@usc.edu>
2013-11-20 17:49:15 -0800

    Added animation panels to the housing gui.

---
+++ src/housing/gui/HousingGui.java
@@ -0,0 +1,13 @@
+package housing.gui;
+
+import gui.MainFrame;
+
+import javax.swing.JPanel;
+
+public class HousingGui extends JPanel {
+	
+	public HousingGui(MainFrame main) {
+		// TODO Auto-generated constructor stub
+//		this.setSize(main.getBuildingViewPanel().getDim());
+	}
+}

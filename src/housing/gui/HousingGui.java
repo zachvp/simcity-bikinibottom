85a434b85cdb1894210d359a7be73e4ff52880e9
Zach Vega-Perkins <vegaperk@usc.edu>
2013-11-21 01:53:57 -0800

    Set up animation testing files for housing gui.

--- src/housing/gui/HousingGui.java
+++ src/housing/gui/HousingGui.java
@@ -1,13 +1,47 @@
 package housing.gui;
 
-import gui.MainFrame;
+import housing.ResidentRole;
 
-import javax.swing.JPanel;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HousingGui extends JPanel {
+import javax.swing.JFrame;
+
+import agent.PersonAgent;
+import agent.Role;
+import agent.gui.AnimationPanel;
+
+public class HousingGui extends JFrame {
+	/* --- Keeps track of all the elements --- */
+	List<PersonAgent> people = new ArrayList<PersonAgent>();
 	
-	public HousingGui(MainFrame main) {
+	PersonAgent residentPerson = new PersonAgent("Resident");
+	ResidentRole residentRole = new ResidentRole(residentPerson);
+	ResidentGui residentGui = new ResidentGui(residentRole);
+	
+	public HousingGui() {
 		// TODO Auto-generated constructor stub
 //		this.setSize(main.getBuildingViewPanel().getDim());
+		AnimationPanel housingAnimationPanel = new AnimationPanel();
+		this.add(housingAnimationPanel);
+		
+		/* -- Add to Lists --- */
+		people.add(residentPerson);
+		startAndActivate(residentPerson, residentRole);
+		housingAnimationPanel.addGui(residentGui);
 	}
+	
+	private void startAndActivate(PersonAgent agent, Role role){
+		agent.startThread();
+		agent.addRole(residentRole);
+		role.activate();
+	}
+	
+	public static void main(String[] args) {
+		HousingGui gui = new HousingGui();
+		gui.setTitle("Housing Panel");
+		gui.setVisible(true);
+		gui.setResizable(false);
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
---
+++ src/housing/gui/ResidentGui.java
@@ -0,0 +1,79 @@
+package housing.gui;
+
+import housing.interfaces.Resident;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import agent.gui.Gui;
+
+public class ResidentGui implements Gui {
+
+	private Resident resident = null;
+	private boolean canRelease = false;//this prevents excessive releases from occurring
+	private boolean waiting = false;//checks if resident is going to be idle
+
+	String orderName="Test";//text of customer order
+	private boolean holdingOrder = false;
+
+	private int xPos = 20, yPos = 20;//default resident position
+	private int xDestination = 50, yDestination = 50;//default start position
+
+	public ResidentGui(Resident resident) {
+		this.resident = resident;
+	}
+
+	public void updatePosition() {
+		if (xPos < xDestination)
+			xPos++;
+		else if (xPos > xDestination)
+			xPos--;
+
+		if (yPos < yDestination)
+			yPos++;
+		else if (yPos > yDestination)
+			yPos--;
+
+		if (xPos == xDestination && yPos == yDestination && canRelease && !waiting ) {
+			canRelease = false;
+//			agent.msgAtDest();
+		}
+	}
+
+	public void draw(Graphics2D g) {
+		g.setColor(Color.GRAY);
+		g.fillRect(xPos, yPos, 20, 20);
+	    	
+		if(holdingOrder && orderName != null){
+			g.setColor(Color.BLACK);
+			g.drawString(orderName, xPos, yPos);
+		}
+	}
+
+	public boolean isPresent() {
+		return true;
+	}
+
+	public Resident getAgent(){
+		return resident;
+	}
+
+	public void DoGoToSomeWhere(int x, int y) {
+		waiting = false;
+		xDestination = x + 20;
+		yDestination = y - 20;
+		canRelease = true;
+	}
+
+	public int getXPos() {
+		return xPos;
+	}
+
+	public int getYPos() {
+		return yPos;
+	}
+
+//	public void pause(){
+//		agent.pause();
+//	}
+}

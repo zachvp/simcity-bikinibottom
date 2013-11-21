c3d9d32ce77b59c8cedfbd953b47a5e38628ac49
Erik Strottmann <erik.strottmann@gmail.com>
2013-11-20 20:04:20 -0800

    Removed merge artifacts from Agent and Constants (this time in master branch)

--- src/agent/Agent.java
+++ src/agent/Agent.java
@@ -10,17 +10,10 @@ import java.util.concurrent.*;
 public abstract class Agent {
     Semaphore stateChange = new Semaphore(1, true);//binary semaphore, fair
     private AgentThread agentThread;
-<<<<<<< HEAD
-    private boolean pause = false;
-
-=======
-     
-    private boolean paused = false;
-    Semaphore pausedsem = new Semaphore(0);
     
->>>>>>> market
-    protected Agent() {
-    }
+    private boolean pause = false;
+    
+    protected Agent() {}
 
     /**
      * This should be called whenever state has changed that might cause
@@ -44,11 +37,8 @@ public abstract class Agent {
      * Return agent name for messages.  Default is to return java instance
      * name.
      */
-<<<<<<< HEAD
+    
     public String getName() {
-=======
-    protected String getName() {
->>>>>>> market
         return StringUtil.shortName(this);
     }
 
@@ -104,7 +94,6 @@ public abstract class Agent {
             agentThread = null;
         }
     }
-<<<<<<< HEAD
     
     /**
      * Temporarily pause agent scheduler thread.
@@ -122,12 +111,10 @@ public abstract class Agent {
     	pause = false;
     	stateChanged();
     }
-=======
->>>>>>> market
-
+    
     /**
      * Agent scheduler thread, calls respondToStateChange() whenever a state
-     * change has been signalled.
+     * change has been signaled.
      */
     private class AgentThread extends Thread {
         private volatile boolean goOn = false;
@@ -144,23 +131,14 @@ public abstract class Agent {
                 try {
                     // The agent sleeps here until someone calls, stateChanged(),
                     // which causes a call to stateChange.give(), which wakes up agent.
-<<<<<<< HEAD
-=======
-                	if (paused){
-                		pausedsem.acquire();
-                	}
->>>>>>> market
                     stateChange.acquire();
                     
                     //The next while clause is the key to the control flow.
                     //When the agent wakes up it will call respondToStateChange()
                     //repeatedly until it returns FALSE.
                     //You will see that pickAndExecuteAnAction() is the agent scheduler.
-<<<<<<< HEAD
-                    while (!pause && pickAndExecuteAnAction()) ;
-=======
-                    while (pickAndExecuteAnAction()) ;
->>>>>>> market
+                    while (!pause && pickAndExecuteAnAction());
+                    
                 } catch (InterruptedException e) {
                     // no action - expected when stopping or when deadline changed
                 } catch (Exception e) {
@@ -174,16 +152,5 @@ public abstract class Agent {
             this.interrupt();
         }
     }
-<<<<<<< HEAD
-=======
-    public void pause(){
-    	paused = true;
-    }
-    
-    public void restart(){
-    	paused = false;
-    	pausedsem.release();
-    }
->>>>>>> market
 }
 

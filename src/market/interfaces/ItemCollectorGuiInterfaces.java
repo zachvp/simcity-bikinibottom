7b11a03d9ba68b15130cf4264b8c9d0562d38da9
Anthony Cheung <chinghocheung@hotmail.com>
2013-11-24 23:11:09 -0800

    Added Unit Test for ItemCollector and Fixed the MarketControlPanelUpdate

---
+++ src/market/interfaces/ItemCollectorGuiInterfaces.java
@@ -0,0 +1,36 @@
+package market.interfaces;
+
+import java.awt.Graphics2D;
+
+import agent.gui.Gui;
+import market.gui.MarketInfoPanel;
+
+public interface ItemCollectorGuiInterfaces extends Gui{
+
+	public abstract void setItemCollectorNumber(int i);
+
+	public abstract void setMarketControlPanel(MarketInfoPanel p);
+
+	public abstract void updatePosition();
+
+	public abstract void UpdateInventoryLevel();
+
+	public abstract void GoToWork();
+
+	public abstract void BackReadyStation();
+
+	public abstract void CollectItems();
+
+	public abstract void OffWork();
+
+	public abstract void ContinueOffWork();
+
+	public abstract void draw(Graphics2D g);
+
+	public abstract boolean isPresent();
+
+	public abstract int getXPos();
+
+	public abstract int getYPos();
+
+}
---
+++ src/market/test/mock/MockItemCollectorGui.java
@@ -0,0 +1,91 @@
+package market.test.mock;
+
+
+import java.awt.Graphics2D;
+import java.util.List;
+
+
+
+import market.gui.MarketInfoPanel;
+import market.interfaces.ItemCollector;
+import market.interfaces.ItemCollectorGuiInterfaces;
+
+public class MockItemCollectorGui extends Mock implements ItemCollectorGuiInterfaces {
+
+	public EventLog log = new EventLog();
+	ItemCollector itemCollector;
+	public MockItemCollectorGui(ItemCollector ic) {
+		super("Mock ItemCollector Gui");
+		itemCollector = ic;
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void setItemCollectorNumber(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void setMarketControlPanel(MarketInfoPanel p) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void updatePosition() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void UpdateInventoryLevel() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void GoToWork() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void BackReadyStation() {
+		// TODO Auto-generated method stub
+		log.add(new LoggedEvent("Got the message and go to Home position now "));
+		itemCollector.Ready();
+	}
+	@Override
+	public void CollectItems() {
+		// TODO Auto-generated method stub
+		log.add(new LoggedEvent("Got the message and go to Bench now "));
+		itemCollector.AtCollectStation();
+	}
+	@Override
+	public void OffWork() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void ContinueOffWork() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean isPresent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public int getXPos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int getYPos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}

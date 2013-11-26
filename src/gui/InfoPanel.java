7b9039235b32c76f23d135e8cecd331d41651a11
Erik Strottmann <erik.strottmann@gmail.com>
2013-11-26 09:26:59 -0800

    Fixed MockPerson not overriding inherited abstract methods

--- src/gui/InfoPanel.java
+++ src/gui/InfoPanel.java
@@ -218,7 +218,7 @@ public class InfoPanel extends JPanel implements ActionListener{
 	
 	class PrintTask extends TimerTask {
         public void run() {
-        	displayTime();
+        	getTimeDisplay();
         }
 	}
 

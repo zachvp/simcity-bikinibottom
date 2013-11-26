b3c278c71228ae347cf46eff250e2320f77bcbfe
Victoria Dea <vdea@usc.edu>
2013-11-26 01:34:11 -0800

    added images to bank and market

--- src/bank/gui/LayoutGui.java
+++ src/bank/gui/LayoutGui.java
@@ -3,7 +3,12 @@ package bank.gui;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.concurrent.Semaphore;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 import agent.gui.Gui;
 
@@ -11,7 +16,8 @@ import agent.gui.Gui;
 public class LayoutGui implements Gui{
 
 	private boolean isPresent = true;
-
+	private final int WINDOWX = 600;
+	private final int WINDOWY = 490;
 
 	private int xPos, yPos;
 	private int xDestination, yDestination;
@@ -30,11 +36,22 @@ public class LayoutGui implements Gui{
 
 	boolean canRelease = false;
 
+	BufferedImage image;
+	ImageIcon icon;
+
 	public LayoutGui(){ //HostAgent m) {
 		xPos = deskXPos;
 		yPos = deskYPos;
 		xDestination = deskXPos;
 		yDestination = deskYPos;
+
+		try {
+			image = ImageIO.read(getClass().getResource("bank_floor.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		icon = new ImageIcon(image);
+
 		//		c.msgGotToTeller();
 
 		//		DoGoToTeller(200);
@@ -66,6 +83,9 @@ public class LayoutGui implements Gui{
 	public void draw(Graphics2D g) {//abstract definition, needed for Graphics
 
 		Graphics2D g2 = (Graphics2D)g;
+
+		g2.fillRect(0, 0, WINDOWX, WINDOWY);
+		g2.drawImage(icon.getImage(), 0, 0, null);
 
 		Graphics2D tellerDesk = (Graphics2D)g;
 		tellerDesk.setColor(Color.YELLOW);

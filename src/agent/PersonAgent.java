d899690f2e70e115a296ff36b89d4d3fbbeb1b62
Diego von Beck <dvonbeck@gmail.com>
2013-12-04 16:41:05 -0800

    People won't take buses for now

--- src/agent/PersonAgent.java
+++ src/agent/PersonAgent.java
@@ -203,7 +203,7 @@ public class PersonAgent extends Agent implements Person {
 	 */
 	private void goToLoc(CityLocation loc) {
 		PassengerRole pass = getPassengerRole();
-		pass.msgGoToLocation(loc, true);
+		pass.msgGoToLocation(loc, false);
 		// TODO person is hard coded to be willing to use the bus
 		pass.activate();
 	}

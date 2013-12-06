bde8f6b7da306c8899ff77e3ae95ee8ca88e0d7b
Victoria Dea <vdea@usc.edu>
2013-12-05 23:56:05 -0800

    stuff

--- src/gui/LogDisplay.java
+++ src/gui/LogDisplay.java
@@ -4,7 +4,6 @@ import gui.trace.AlertTag;
 import gui.trace.TracePanel;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -71,31 +70,31 @@ public class LogDisplay extends JPanel implements ActionListener{
 		if (e.getSource() == passenger){
 			if (passenger.getText().equals("Passenger: OFF")){
 				passenger.setText("Passenger: ON");
-				
+				tracePanel.showAlertsWithTag(AlertTag.PASSENGER);
 			}
 			else{
 				passenger.setText("Passenger: OFF");
-
+				tracePanel.hideAlertsWithTag(AlertTag.PASSENGER);
 			}
 		}
 		if (e.getSource() == transportation){
 			if (transportation.getText().equals("Transportation: OFF")){
 				transportation.setText("Transportation: ON");
-
+				tracePanel.showAlertsWithTag(AlertTag.TRANSPORTATION);
 			}
 			else{
 				transportation.setText("Transportation: OFF");
-
+				tracePanel.hideAlertsWithTag(AlertTag.TRANSPORTATION);
 			}
 		}
 		if (e.getSource() == restaurant){
 			if (restaurant.getText().equals("Restaurant: OFF")){
 				restaurant.setText("Restaurant: ON");
-
+				tracePanel.showAlertsWithTag(AlertTag.RESTAURANT);
 			}
 			else{
 				restaurant.setText("Restaurant: OFF");
-
+				tracePanel.hideAlertsWithTag(AlertTag.RESTAURANT);
 			}
 		}
 		if (e.getSource() == bank){
@@ -111,21 +110,21 @@ public class LogDisplay extends JPanel implements ActionListener{
 		if (e.getSource() == market){
 			if (market.getText().equals("Market: OFF")){
 				market.setText("Market: ON");
-
+				tracePanel.showAlertsWithTag(AlertTag.MARKET);
 			}
 			else{
 				market.setText("Market: OFF");
-
+				tracePanel.hideAlertsWithTag(AlertTag.MARKET);
 			}
 		}
 		if (e.getSource() == housing){
 			if (housing.getText().equals("Housing: OFF")){
 				housing.setText("Housing: ON");
-
+				tracePanel.showAlertsWithTag(AlertTag.HOUSING);
 			}
 			else{
 				housing.setText("Housing: OFF");
-
+				tracePanel.hideAlertsWithTag(AlertTag.HOUSING);
 			}
 		}
 	}

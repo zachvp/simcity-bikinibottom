d10e45c7e1b5b121e94575ae7bd5a01377ae1079
Victoria Dea <vdea@usc.edu>
2013-12-05 23:29:45 -0800

    all buttons toggle

--- src/gui/LogDisplay.java
+++ src/gui/LogDisplay.java
@@ -1,5 +1,6 @@
 package gui;
 
+import gui.trace.AlertTag;
 import gui.trace.TracePanel;
 
 import java.awt.BorderLayout;
@@ -25,7 +26,7 @@ public class LogDisplay extends JPanel implements ActionListener{
 		d = new Dimension(Constants.INFO_PANEL_WIDTH, Constants.INFO_PANEL_HEIGHT);
 		setLayout(new BorderLayout());
 		
-		Dimension logDim = new Dimension((int)(Constants.INFO_PANEL_WIDTH*0.80), Constants.INFO_PANEL_HEIGHT);
+		Dimension logDim = new Dimension((int)(Constants.INFO_PANEL_WIDTH*0.78), Constants.INFO_PANEL_HEIGHT);
 		logPanel = new JPanel();
 		logPanel.setPreferredSize(logDim);
 		logPanel.setMaximumSize(logDim);
@@ -33,19 +34,19 @@ public class LogDisplay extends JPanel implements ActionListener{
 		tracePanel.setPreferredSize(logDim);
 		logPanel.add(tracePanel);
 		
-		Dimension buttonDim = new Dimension((int)(Constants.INFO_PANEL_WIDTH*0.20), Constants.INFO_PANEL_HEIGHT);
+		Dimension buttonDim = new Dimension((int)(Constants.INFO_PANEL_WIDTH*0.22), Constants.INFO_PANEL_HEIGHT);
 		buttonsPanel = new JPanel();
 		buttonsPanel.setPreferredSize(buttonDim);
 		buttonsPanel.setMaximumSize(buttonDim);
 		buttonsPanel.setMinimumSize(buttonDim);
 		buttonsPanel.setLayout(new GridLayout(6,1,1,1));
 		
-		passenger = new JButton("Passenger");
-		transportation = new JButton("Transportation");
-		restaurant = new JButton("Restaurant");
-		bank = new JButton("Bank");
-		market = new JButton("Market");
-		housing = new JButton("Housing");
+		passenger = new JButton("Passenger: OFF");
+		transportation = new JButton("Transportation: OFF");
+		restaurant = new JButton("Restaurant: OFF");
+		bank = new JButton("Bank: OFF");
+		market = new JButton("Market: OFF");
+		housing = new JButton("Housing: OFF");
 		
 		passenger.addActionListener(this);
 		transportation.addActionListener(this);
@@ -67,8 +68,65 @@ public class LogDisplay extends JPanel implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+		if (e.getSource() == passenger){
+			if (passenger.getText().equals("Passenger: OFF")){
+				passenger.setText("Passenger: ON");
+				
+			}
+			else{
+				passenger.setText("Passenger: OFF");
 
+			}
+		}
+		if (e.getSource() == transportation){
+			if (transportation.getText().equals("Transportation: OFF")){
+				transportation.setText("Transportation: ON");
+
+			}
+			else{
+				transportation.setText("Transportation: OFF");
+
+			}
+		}
+		if (e.getSource() == restaurant){
+			if (restaurant.getText().equals("Restaurant: OFF")){
+				restaurant.setText("Restaurant: ON");
+
+			}
+			else{
+				restaurant.setText("Restaurant: OFF");
+
+			}
+		}
+		if (e.getSource() == bank){
+			if (bank.getText().equals("Bank: OFF")){
+				bank.setText("Bank: ON");
+				tracePanel.showAlertsWithTag(AlertTag.BANK);
+			}
+			else{
+				bank.setText("Bank: OFF");
+				tracePanel.hideAlertsWithTag(AlertTag.BANK);
+			}
+		}
+		if (e.getSource() == market){
+			if (market.getText().equals("Market: OFF")){
+				market.setText("Market: ON");
+
+			}
+			else{
+				market.setText("Market: OFF");
+
+			}
+		}
+		if (e.getSource() == housing){
+			if (housing.getText().equals("Housing: OFF")){
+				housing.setText("Housing: ON");
+
+			}
+			else{
+				housing.setText("Housing: OFF");
+
+			}
+		}
+	}
 }

23badfbca296d7f5dce29b51c096deb60d352610
Anthony Cheung <chinghocheung@hotmail.com>
2013-12-06 22:02:15 -0800

    Removed the function msgNoItem in the DeliveryReceiverInterface

--- src/market/interfaces/DeliveryReceiver.java
+++ src/market/interfaces/DeliveryReceiver.java
@@ -7,8 +7,6 @@ import agent.WorkRole;
 
 public interface DeliveryReceiver {
 
-	public abstract void msgNoItem();
-	
 	public abstract void msgHereIsYourItems(List<Item> DeliverList);
 	
 	public abstract void msgHereIsMissingItems(List<Item> MissingItemList);

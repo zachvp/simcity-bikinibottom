d4afdf0d55bfe10d6b7ae7305d22a485758c8dc3
Erik Strottmann <erik.strottmann@gmail.com>
2013-12-10 16:06:17 -0800

    Fixed compile errors people pushed to master...

--- src/restaurant/vdea/test/CashierTest.java
+++ src/restaurant/vdea/test/CashierTest.java
@@ -32,7 +32,8 @@ public class CashierTest extends TestCase
 	 */
 	public void setUp() throws Exception{
 		super.setUp();		
-		cashier = new CashierRole("cashier", 200);		
+		// cashier = new CashierRole("cashier", 200);
+		cashier = new CashierRole(null, null); // TODO ERIK WAS HERE, FIXING YOUR COMPILE ERRORS
 		customer = new MockCustomer("mockcustomer");		
 		badCustomer = new MockCustomer("BADcustomer");
 		waiter = new MockWaiter("mockwaiter");
